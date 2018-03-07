4216f084655b9b3064552197be6ff92792444fba
1367356 <1185978642@qq.com>
2018-03-07 15:38:35 +0800

    background build.gradle配置完成

---
+++ shop_background/src/main/java/com/background/SpringBackgrouondMainClass.java
@@ -0,0 +1,12 @@
+package com.background;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackgrouondMainClass {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBackgrouondMainClass.class,args);
+    }
+}
